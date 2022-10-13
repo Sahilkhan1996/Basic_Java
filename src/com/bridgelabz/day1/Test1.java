72bb00443d030a1b1265b2546069fdce6c5ca66c
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-10-13 20:15:45 +0530

    Added:UC1-New Feature of java

--- src/com/bridgelabz/day1/Test1.java
+++ src/com/bridgelabz/day1/Test1.java
@@ -2,6 +2,7 @@ package com.bridgelabz.day1;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("Welcom to basics of java");
+		System.out.println("Welcome to basics of java");
+		System.out.println("Modifiying the code for the first time");
 	}
 }
